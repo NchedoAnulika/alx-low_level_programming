5a1bd14565afd31bdc8afa25ef664a2306e456be
NchedoAnulika <nchedo.arinze@gmail.com>
2023-08-10 15:13:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main= The entry point
+ *
+ * return=returns the integer 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
