f6e8b2f5d41567471c71fe939a04fede49521348
NchedoAnulika <nchedo.arinze@gmail.com>
2023-08-17 17:29:14 -0700

    101-print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,7 +2,8 @@
 
 /**
  * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * @n: The integer to be printed
+ *
  */
 void print_number(int n)
 {
@@ -11,7 +12,7 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 != 0)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
