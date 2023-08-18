3dc9c23b618871e30a4d4574c5c9e010a242ee8a
NchedoAnulika <nchedo.arinze@gmail.com>
2023-08-17 17:56:00 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,25 +9,28 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100;)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%i", i);
 		}
-		i++;
+		if (i < 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
