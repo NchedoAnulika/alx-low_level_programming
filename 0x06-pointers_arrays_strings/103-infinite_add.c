1757899ea88e28329d94995e4b27fe1989f173cc
NchedoAnulika <nchedo.arinze@gmail.com>
2023-08-24 18:57:12 -0700

    103-add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,39 +11,44 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, sum;
-	int i = size_r - 2, len1 = 0, len2 = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (n1[len1])
-		len1++;
-	while (n2[len2])
-		len2++;
-
-	while (i >= 0 && (len1 > 0 || len2 > 0 || carry))
-	{
-		if (len1 > 0)
-		{
-			sum = n1[--len1] - '0';
-		}
-		if (len2 > 0)
-		{
-			sum += n2[--len2] - '0';
-		}
-		sum += carry;
-
-		carry = sum / 10;
-		r[i] = (sum % 10) + '0';
-		i--;
-	}
-
-	if (carry > 0 && i >= 0)
-	{
-		r[i] = carry + '0';
-	}
-	else if (carry > 0)
-	{
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
+	{
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
-
-	return (i < 0 ? 0 : r + i + 1);
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
