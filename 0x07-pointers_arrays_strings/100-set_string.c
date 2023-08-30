5a697674e3f648579b70cdf4896dc4887a421d21
NchedoAnulika <nchedo.arinze@gmail.com>
2023-08-30 08:44:32 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - Update pointer value to char.
+ * @s: Pointer to pointer.
+ * @to: Pointer to char.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
