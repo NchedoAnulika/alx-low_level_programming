542c93455e831d44eee9f898526833809f5b48d5
NchedoAnulika <nchedo.arinze@gmail.com>
2023-08-30 20:36:08 -0700

    100-palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,22 +5,20 @@
  * @s: The string to check.
  * @start: The starting index.
  * @end: The ending index.
- *
  * Return: 1 if palindrome, 0 otherwise.
  */
 int is_palindrome_recursive(char *s, int start, int end)
 {
 	if (start >= end)
 		return (1);
-	if (s[start] != s[end])
-		return (0);
-	return (is_palindrome_recursive(s, start + 1, end - 1));
+	return ((s[start] != s[end]) ? 0 : is_palindrome_recursive
+		(s, start + 1, end - 1));
 }
 /**
- * is_palindrome - Checks if a string is palindrome.
+ * is_palindrome - Checks if a string is a palindrome.
  * @s: The string to check.
  *
- * Return: 1 if s is palindrome, 0 otherwise.
+ * Return: 1 if s is a palindrome, 0 otherwise.
  */
 int is_palindrome(char *s)
 {
